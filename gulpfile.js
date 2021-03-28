d117f9d43138dd0b3e8d6575b7fa3b9fdef47c60
tianheg <yidajiabei@gmail.com>
2021-03-28 18:05:59 +0800

    [gulpfile.js] update

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,57 @@
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var minifycss = require("gulp-minify-css");
-var uglify = require("gulp-uglify");
-var htmlmin = require("gulp-htmlmin");
-var htmlclean = require("gulp-htmlclean");
-var babel = require("gulp-babel");
-var connect = require("gulp-connect");
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const minifycss = require('gulp-minify-css')
+const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const htmlclean = require('gulp-htmlclean')
+const babel = require('gulp-babel')
+const connect = require('gulp-connect')
 
 // 压缩 css 文件
-gulp.task("css", function css(done) {
+gulp.task('css', function css (done) {
   gulp
-    .src("./public/css/*.css")
+    .src('./public/css/*.css')
     .pipe(minifycss())
-    .pipe(gulp.dest("./public/css"))
-    .pipe(connect.reload());
-  done();
-});
+    .pipe(gulp.dest('./public/css'))
+    .pipe(connect.reload())
+  done()
+})
 
 // 压缩 html 文件
-gulp.task("html", function html(done) {
+gulp.task('html', function html (done) {
   gulp
-    .src("./public/**/*.html")
+    .src('./public/**/*.html')
     .pipe(htmlclean())
     .pipe(
       htmlmin({
         removeComments: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true,
+        minifyURLs: true
       })
     )
-    .pipe(gulp.dest("./public"))
-    .pipe(connect.reload());
-  done();
-});
+    .pipe(gulp.dest('./public'))
+    .pipe(connect.reload())
+  done()
+})
 
 // 压缩 js 文件
-gulp.task("js", function js(done) {
+gulp.task('js', function js (done) {
   gulp
-    .src("./public/js/*.js")
+    .src('./public/js/*.js')
     .pipe(
       babel({
-        presets: ["@babel/preset-env"],
+        presets: ['@babel/preset-env']
       })
     )
     .pipe(uglify())
-    .on("error", function (err) {
-      gutil.log(gutil.colors.red("[Error]"), err.toString());
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString())
     })
-    .pipe(gulp.dest("./public/js"))
-    .pipe(connect.reload());
-  done();
-});
+    .pipe(gulp.dest('./public/js'))
+    .pipe(connect.reload())
+  done()
+})
 
 // 默认任务
-gulp.task("default", gulp.series(gulp.parallel("html", "css", "js")));
+gulp.task('default', gulp.series(gulp.parallel('html', 'css', 'js')))
