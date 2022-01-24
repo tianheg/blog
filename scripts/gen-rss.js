3bd7cdeea327be5a014dd48b20baeada5943aeb1
tianheg <app@tianheg.co>
2022-01-24 19:02:01 +0800

    BUILD HTML

---
+++ scripts/gen-rss.js
@@ -0,0 +1,37 @@
+import { Feed } from "feed";
+
+import moment from "moment-timezone";
+
+const feed = new Feed({
+  title: "Feed Title",
+  description: "This is my personal feed!",
+  id: "http://example.com/",
+  link: "http://example.com/",
+  language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+  image: "http://example.com/image.png",
+  favicon: "http://example.com/favicon.ico",
+  copyright: "All rights reserved 2013, John Doe",
+  updated: new Date(), // optional, default = today
+  generator: "awesome", // optional, default = 'Feed for Node.js'
+  feedLinks: {
+    json: "https://example.com/json",
+    atom: "https://example.com/atom",
+  },
+  author: {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    link: "https://example.com/johndoe",
+  },
+});
+
+feed.addCategory("Technologie");
+
+feed.addContributor({
+  name: "Johan Cruyff",
+  email: "johancruyff@example.com",
+  link: "https://example.com/johancruyff",
+});
+
+console.log(feed.rss2());
+// Output: RSS 2.0
+console.log(moment().tz("Asia/Shanghai").format());
