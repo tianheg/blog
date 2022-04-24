09a8e6221e5d915dbe251dcd5fd84156e9431e91
tianheg <app@tianheg.co>
2022-04-24 14:30:15 +0800

    BREAKING CHANGE: add comments learn from https://yihui.org/cn/ https://cyrusyip.org/donate/

---
+++ static/js/custom-disqus.js
@@ -0,0 +1,13 @@
+const comment = document.querySelector('section.comments')
+const full = comment.classList.contains('comment-full')
+
+if (document.querySelector('#layout-2').checked == true) {
+  comment.classList.remove('comments')
+  comment.classList.add('comment-full')
+} else {
+  comment.classList.remove('comment-full')
+}
+
+console.log(comment)
+// https://cdn.jsdelivr.net/npm/@xiee/utils/js/custom-disqus.js
+// TODO lazy loading
