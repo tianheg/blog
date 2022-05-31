f513d292afc9af2f1f7063941c6e84b5278528fc
tianheg <app@tianheg.co>
2022-05-31 13:03:20 +0800

    add time

--- themes/tianheg/assets/js/header.js
+++ themes/tianheg/assets/js/header.js
@@ -34,6 +34,21 @@ window.addEventListener(
       );
       {{ end }}
     }
+
+    const timeEl = document.querySelector('.time')
+
+    function setTime() {
+      const time = new Date()
+      const hours = time.getHours()
+      const minutes = time.getMinutes()
+
+      timeEl.innerHTML = `${hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }`
+    }
+
+    setTime()
+    setInterval(setTime, 1000)
   },
   { once: true }
 );
