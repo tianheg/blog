7b0caf9f2fc2dd906e3723c7886649472f03eb9c
tianheg <yidajiabei@gmail.com>
2023-05-15 17:35:14 +0800

    use Node.js pure fetch not node-fetch

--- functions/unsplash.js
+++ functions/unsplash.js
@@ -1,9 +1,8 @@
 import serverless from "serverless-http"
 import { createApi } from "unsplash-js";
-import nodeFetch from 'node-fetch';
 import express from 'express'
 
-const unsplash = createApi({ accessKey: process.env.ACCESS_KEY, fetch: nodeFetch })
+const unsplash = createApi({ accessKey: process.env.ACCESS_KEY, fetch: fetch })
 
 const app = express()
 const PORT = 3000
