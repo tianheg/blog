5fb8ac6c9424215126b44ae994eba8b86ff6f6fd
tianheg <app@tianheg.co>
2023-05-15 19:09:05 +0800

    Use node-fetch again

--- functions/unsplash.js
+++ functions/unsplash.js
@@ -1,18 +1,21 @@
-import serverless from "serverless-http"
-import { createApi } from "unsplash-js";
+import serverless from 'serverless-http'
+import { createApi } from 'unsplash-js'
+import nodeFetch from 'node-fetch'
 import express from 'express'
 
-const unsplash = createApi({ accessKey: process.env.ACCESS_KEY, fetch: fetch })
+const unsplash = createApi({
+  accessKey: process.env.ACCESS_KEY,
+  fetch: nodeFetch
+})
 
 const app = express()
 const PORT = 3000
 
 app.get('/.netlify/functions/unsplash/', (req, res) => {
-  unsplash.photos.getRandom()
-    .then(json => {
-      let imageUrl = json.response.urls.regular;
-      res.send(`<img src="${imageUrl}">`)
-    })
+  unsplash.photos.getRandom().then((json) => {
+    let imageUrl = json.response.urls.regular
+    res.send(`<img src="${imageUrl}">`)
+  })
 })
 
 app.listen(PORT, () => {
