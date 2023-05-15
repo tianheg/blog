f88d8ce0d48e48479ef74f1e2b1e7dbc88e12494
tianheg <app@tianheg.co>
2023-05-15 18:25:44 +0800

    change another way to downloda dart-sass-embedded

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+## pagefind
 wget -q https://github.com/CloudCannon/pagefind/releases/download/v$PAGEFIND_VERSION/pagefind_extended-v$PAGEFIND_VERSION-x86_64-unknown-linux-musl.tar.gz -O - | tar -xz
 mv pagefind_extended /opt/build/repo/node_modules/.bin
-cp /opt/build/repo/node_modules/sass-embedded-linux-x64/dart-sass-embedded/dart-sass-embedded /opt/build/repo/node_modules/.bin/src
+## dart-sass-embedded
+wget -q https://github.com/sass/dart-sass-embedded/releases/download/$DART_SASS_VERSION/sass_embedded-$DART_SASS_VERSION-linux-x64.tar.gz -O - | tar -xz
+mv dart-sass-embedded /opt/build/repo/node_modules/.bin
 dart-sass-embedded --version
 pnpm run all
