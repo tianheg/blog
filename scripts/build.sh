8136f2be406b75b9b99d46bc661272ae17b6fdf6
tianheg <app@tianheg.co>
2023-05-15 18:02:54 +0800

    Recover cp dart-sass-embedded

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 wget -q https://github.com/CloudCannon/pagefind/releases/download/v$PAGEFIND_VERSION/pagefind_extended-v$PAGEFIND_VERSION-x86_64-unknown-linux-musl.tar.gz -O - | tar -xz
 mv pagefind_extended /opt/build/repo/node_modules/.bin
-#cp /opt/build/repo/node_modules/sass-embedded-linux-x64/dart-sass-embedded/dart-sass-embedded /opt/build/repo/node_modules/.bin
+cp /opt/build/repo/node_modules/sass-embedded-linux-x64/dart-sass-embedded/dart-sass-embedded /opt/build/repo/node_modules/.bin
 dart-sass-embedded --version
 pnpm run all
