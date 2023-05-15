75c1fa6f81158a2bb912cabdc569fb5fee154ead
tianheg <yidajiabei@gmail.com>
2023-05-15 18:39:11 +0800

    Fix path error

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## pagefind
-wget -q https://github.com/CloudCannon/pagefind/releases/download/v$PAGEFIND_VERSION/pagefind_extended-v$PAGEFIND_VERSION-x86_64-unknown-linux-musl.tar.gz -O - | tar -xz
-mv pagefind_extended /opt/build/repo/node_modules/.bin
+wget -q https://github.com/CloudCannon/pagefind/releases/download/v$PAGEFIND_VERSION/pagefind_extended-v$PAGEFIND_VERSION-x86_64-unknown-linux-musl.tar.gz -O - | tar -xz -C /opt/build/repo/node_modules/.bin
 ## dart-sass-embedded
-wget -q https://github.com/sass/dart-sass-embedded/releases/download/$DART_SASS_VERSION/sass_embedded-$DART_SASS_VERSION-linux-x64.tar.gz -O - | tar -xz
-mv sass_embedded/* /opt/build/repo/node_modules/.bin
+wget -q https://github.com/sass/dart-sass-embedded/releases/download/$DART_SASS_VERSION/sass_embedded-$DART_SASS_VERSION-linux-x64.tar.gz -O - | tar -xz -C /opt/build/repo/node_modules/.bin
+pwd
+mv /opt/build/repo/node_modules/.bin/sass_embedded/* /opt/build/repo/node_modules/.bin
 dart-sass-embedded --version
 pnpm run all
