af917c027863297845ec676d2649cc44159a8515
tianheg <app@tianheg.co>
2024-02-25 10:08:32 +0000

    Update Hugo setup script to download and install Hugo in a local directory

--- scripts/hugo-setup.sh
+++ scripts/hugo-setup.sh
@@ -2,13 +2,7 @@
 
 HUGO_VERSION="0.123.0"
 HUGO_ID="hugo_extended_${HUGO_VERSION}"
-HOME="archie"
-HUGO_DIR="/home/${HOME}/Downloads/hugo-ex"
-
-pwd
-rm -rf "${HUGO_DIR}"
-mkdir "${HUGO_DIR}"
-wget -O - "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}_Linux-64bit.tar.gz" | tar -xz -C "${HUGO_DIR}"
-mv "${HUGO_DIR}/hugo" /usr/bin/hugo
-rm -rf "${HUGO_DIR}"
-pwd
+mkdir -p ./hugo-bin
+wget -O - "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}_Linux-64bit.tar.gz" | tar -xz -C ./hugo-bin
+mv ./hugo-bin/hugo /usr/bin/
+rm -rf ./hugo-bin
