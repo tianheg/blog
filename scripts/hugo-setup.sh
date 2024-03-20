a9f20c2b09238c2ef6b51093964f323c1d233d9d
tianheg <i@tianheg.co>
2024-03-20 11:24:11 +0800

    upgrade Hugo version to 0.124.0

--- scripts/hugo-setup.sh
+++ scripts/hugo-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-HUGO_VERSION="0.123.0"
+HUGO_VERSION="0.124.0"
 HUGO_ID="hugo_extended_${HUGO_VERSION}"
 mkdir -p ./hugo-bin
 wget -O - "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}_Linux-64bit.tar.gz" | tar -xz -C ./hugo-bin
-mv ./hugo-bin/hugo /usr/bin/
+mv ./hugo-bin/hugo ~/.local/bin/
 rm -rf ./hugo-bin
