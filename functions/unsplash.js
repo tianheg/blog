9832fcd98fdc2c6de410ab907a79ee75555d42a2
tianheg <app@tianheg.co>
2024-04-03 11:06:23 +0800

    update unsplash function to get only img url

--- functions/unsplash.js
+++ functions/unsplash.js
@@ -1,29 +1,32 @@
-import { createApi } from 'unsplash-js'
-import fetch from 'node-fetch'
-import express from 'express'
-import serverless from 'serverless-http'
+import { createApi } from "unsplash-js";
+import fetch from "node-fetch";
+import express from "express";
+import serverless from "serverless-http";
 
 const unsplash = createApi({
   accessKey: process.env.ACCESS_KEY,
-  fetch
-})
+  fetch,
+});
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/.netlify/functions/unsplash/', async (req, res) => {
+app.get("/.netlify/functions/unsplash/", async (req, res) => {
   try {
-    const response = await unsplash.photos.getRandom()
-    const imageUrl = response.response.urls.regular
-    res.send(`${imageUrl}`)
+    const response = await unsplash.photos.getRandom();
+    const imageUrl = response.response.urls.regular;
+    const imageResponse = await fetch(imageUrl);
+    const imageBuffer = Buffer.from(await imageResponse.arrayBuffer());
+    res.set("Content-Type", "image/jpeg");
+    res.send(imageBuffer);
   } catch (error) {
-    console.error(error)
-    res.status(500).send('An error occurred while fetching the image.')
+    console.error(error);
+    res.status(500).send("An error occurred while fetching the image.");
   }
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Unsplash app listening on port ${PORT}`)
-})
+  console.log(`Unsplash app listening on port ${PORT}`);
+});
 
-exports.handler = serverless(app)
+exports.handler = serverless(app);
