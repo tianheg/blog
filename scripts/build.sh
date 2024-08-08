e5c85912b153b2e537a83352bc5bc59eb843691a
tianheg <app@tianheg.co>
2024-08-09 06:44:06 +0800

    rm useless files

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,5 @@
 
 ## pagefind
 wget -q https://github.com/CloudCannon/pagefind/releases/download/v$PAGEFIND_VERSION/pagefind_extended-v$PAGEFIND_VERSION-x86_64-unknown-linux-musl.tar.gz -O - | tar -xz -C ./node_modules/.bin
-## dart-sass embedded version
-mkdir -p ./bin
-wget -q https://github.com/sass/dart-sass/releases/download/$DART_SASS_VERSION/dart-sass-$DART_SASS_VERSION-linux-x64.tar.gz -O - | tar -xz -C ./bin
-export PATH=./bin/dart-sass:$PATH
-sass --embedded --version
+## build
 bun run all
