737731a6fe00a81a5fdcb4b14cc55e8fd5c8efa0
tianheg <i@tianheg.co>
2025-06-24 23:30:07 +0800

    fix: hugo setup script

--- scripts/hugo-setup.sh
+++ scripts/hugo-setup.sh
@@ -4,5 +4,6 @@ HUGO_VERSION="0.147.8"
 HUGO_ID="hugo_extended_${HUGO_VERSION}"
 mkdir -p ./hugo-bin
 wget -O - "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}_Linux-64bit.tar.gz" | tar -xz -C ./hugo-bin
+mkdir -p ~/.local/bin
 mv ./hugo-bin/hugo ~/.local/bin/
 rm -rf ./hugo-bin
