1dc2a69655009ffdd1355820141f08eae18ecf03
tianheg <i@tianheg.co>
2025-06-25 23:32:28 +0800

    chore: rm files not usefull anymore

--- scripts/local-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -d "public" && -d "resources" ]]; then
-  echo "Directories exist"
-  trash public && trash resources && hugo server
-else
-  echo "Directories do not exist"
-fi
