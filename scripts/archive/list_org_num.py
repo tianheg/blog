3a627891ead96fca588ee288e4a5607e6117e49a
tianheg <app@tianheg.co>
2025-06-25 23:32:28 +0800

    chore: rm files not usefull anymore

--- scripts/archive/list_org_num.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-print(len([name for name in os.listdir("/home/archie/repo/blog/org/")]))
-
-# https://stackoverflow.com/a/2632251
--- scripts/md2org-file2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-while IFS= read -r line
-do
-  # Run the Python function for each file path
-  python -c "import sys; sys.path.insert(0, './scripts'); import md2org_frontmatter; md2org_frontmatter.minify_front_matter('$line'); md2org_frontmatter.convert_to_org_mode('$line')"
-done < "+++file.txt"
