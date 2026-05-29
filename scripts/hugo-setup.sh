f972077b491e0e614fb18c09bc0db3c3804c2fd9
tianheg <app@tianheg.co>
2026-05-29 10:52:07 +0200

    chore: bump Hugo to v0.162.1 in build scripts

--- scripts/hugo-setup.sh
+++ scripts/hugo-setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-HUGO_VERSION="0.161.0"
+HUGO_VERSION="0.162.1"
 HUGO_ID="hugo_${HUGO_VERSION}"
 mkdir -p ./hugo-bin
-wget -O - "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}_Linux-64bit.tar.gz" | tar -xz -C ./hugo-bin
+wget -O - "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}_linux-amd64.tar.gz" | tar -xz -C ./hugo-bin
 mkdir -p ~/.local/bin
 mv ./hugo-bin/hugo ~/.local/bin/
 rm -rf ./hugo-bin
